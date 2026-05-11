7f97b9408700d60fdcd42a08e115ed00ac5a9b34
Priyadharsini RK <156496813+PriyadharsiniRK@users.noreply.github.com>
2026-05-11 21:55:28 +0200

    Add files via upload

---
+++ test.js
@@ -0,0 +1,26 @@
+﻿
+console.log("JavaScript Loaded");
+alert("JavaScript Connected");
+function filterEvenNumbers(numbers) {
+    return numbers.filter(number => number % 2 !== 0);
+}
+
+let result = filterEvenNumbers([1, 2, 3, 4, 5, 6]);
+
+console.log(result);
+
+
+function celsiusToFahrenheit(celsius) {
+    return (celsius * 9 / 5) + 32;
+}
+
+function fahrenheitToCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+}
+
+console.log("50°C is " + celsiusToFahrenheit(50) + "°F");
+
+console.log("78°F is " + fahrenheitToCelsius(78).toFixed(3) + "°C");
+
+
+
